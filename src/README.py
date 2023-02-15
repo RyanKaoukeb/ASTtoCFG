0c411e5949bc059c0d942f478055e8eb9b1b00e9
RyanKaoukeb <ryan.kaoukeb@gmail.com>
2023-02-15 15:32:53 -0500

    envmodif

--- src/README.py
+++ src/README.py
@@ -2,9 +2,9 @@ from ASTtoCFGVisitor import ASTtoCFGVisitor
 from code_analysis import CFGReader, ASTReader
 cfgreader = CFGReader()
 astreader = ASTReader()
-ast = astreader.read_ast("../part_1/functioncall/functioncall.php.ast.json")
+ast = astreader.read_ast("../part_1/while/while.php.ast.json")
 
 atcV = ASTtoCFGVisitor()
 cfg = atcV.visit(ast)
 
-cfg.show(filename="function.cfg.dot")
+cfg.show(filename="while.cfg.dot")
