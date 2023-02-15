bc39e69c8ec8a7c273e52865cf75f399f4a17695
Serp <manu.serpette74@gmail.com>
2023-02-15 16:16:14 -0500

    deadcodefunc+breakcontinue

--- src/README.py
+++ src/README.py
@@ -3,14 +3,17 @@ from deadcodeVisitor import DeadcodeVisitor
 from code_analysis import CFGReader, ASTReader
 cfgreader = CFGReader()
 astreader = ASTReader()
-ast = astreader.read_ast("../part_1/functioncall/functioncall.php.ast.json")
+# ast = astreader.read_ast("../part_1/while/while.php.ast.json")
+# ast = astreader.read_ast(
+#     "../while_continue_break/while_continue_break.php.ast.json")
+ast = astreader.read_ast("../part_2/code_mort/example3.php.ast.json")
 
 atcV = ASTtoCFGVisitor()
 cfg = atcV.visit(ast)
 
-cfg = cfgreader.read_cfg("../part_2/code_mort/example3.php.cfg.json")
+#cfg = cfgreader.read_cfg("../part_2/code_mort/example3.php.cfg.json")
 cfg.show(filename="test.cfg.dot")
-print(cfg.get_type(3))
 dcv = DeadcodeVisitor()
 
-print(dcv.visit(cfg))
+dcv.visit(cfg)
+dcv.visit_def(ast)
--- src/deadcodeVisitor.py
+++ src/deadcodeVisitor.py
@@ -1,10 +1,11 @@
 
-from code_analysis import CFG, CFGException
+from code_analysis import CFG, CFGException, AST
 
 
 class DeadcodeVisitor:
     def __init__(self):
         self.cfg = None
+        self.ast = None
         self.deadcode = []
         self.alivecode = []
         self.allcode = None
@@ -31,5 +32,32 @@ class DeadcodeVisitor:
         for node in self.allcode:
             if node not in self.alivecode:
                 self.deadcode.append(node)
-
+        print("Deadcode: ", self.deadcode)
         return self.deadcode
+
+    def visit_def(self, ast: AST):
+        self.ast = ast
+        rootAST = self.ast.get_root()
+
+        fifo = [rootAST]
+        func_def = []
+        func_call = []
+
+        while len(fifo) > 0:
+            node = fifo.pop()
+            if self.ast.get_type(node) == "FunctionCall":
+                func_call.append(node)
+            if self.ast.get_type(node) == "FunctionStatement":
+                func_def.append(node)
+            for child in self.ast.get_children(node):
+                fifo.append(child)
+
+        for node in func_def:
+            name = self.ast.get_image(node)
+            flag = True
+            for call in func_call:
+                if self.ast.get_image(call) == name:
+                    flag = False
+            if flag:
+                print("Warning: Function " + name +
+                      " is defined but never called")
